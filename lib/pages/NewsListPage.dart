db1f19e23d362f4d1985dad65a3c2562d9604bfc
zengzhuoyu <zengzhuoyu24@163.com>
2018-08-06 07:00:28 +0800

    Initial commit

---
+++ lib/pages/NewsListPage.dart
@@ -0,0 +1,12 @@
+// pages/NewsListPage.dart
+import 'package:flutter/material.dart';
+
+// 资讯列表页面
+class NewsListPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new Center(
+      child: new Text("NewsListPage"),
+    );
+  }
+}
